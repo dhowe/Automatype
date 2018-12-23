903d68ee773d501ea17d1af7a9c63754d6a83d14
dhowe <daniel@rednoise.org>
2018-12-24 07:35:01 +0800

    fast-write-all-meds

--- writeAllMeds.js
+++ writeAllMeds.js
@@ -1,31 +1,37 @@
 let fs = require('fs');
 //let RiTa = require('rita');
 let RiTa = require('./lib/rita-full');
+let levenshtein = require('fast-levenshtein');
 
 let format = 'cyto';
 let args = process.argv.slice(2);
 let maxAllowedMed = args && Math.min(args[0],3) || 0;
 let keys = new RiTa.RiLexicon().keys;
 let edgeFile = 'edges-dist'+maxAllowedMed+'.csv';
-let words = JSON.parse(fs.readFileSync('allnodes.json', 'utf8'));
+//let words = JSON.parse(fs.readFileSync('allnodes.json', 'utf8'));
 
 if (maxAllowedMed < 1) {
-  console.log('\nUsage: node logmeds [maxMed]');
+  console.log('\nUsage: node writeAllMeds 4');
   return;
 }
 
-let edgeData = "Source,Target,Type\n";
+let edgeData = "Source,Target,Med\n";
 FOR: for (var i = 0; i < keys.length; i++) {
   for (var j = 0; j < keys.length; j++) {
     if (i == j) continue;
-    var word1 = keys[i];
-    var word2 = keys[j];
-    var med = RiTa.minEditDistance(word1, word2);
+    var word1 = keys[i], word2 = keys[j];
+    var med = levenshtein.get(word1, word2);
     if (med <= maxAllowedMed) {
-      if (format != 'cyto')
-        edgeData += words[word1]+','+words[word2] + ",undirected\n";
-      else
-        edgeData += word1+','+word2 + "\n";
+      //if (format != 'cyto')
+       // edgeData += words[word1]+','+words[word2] + ",undirected\n";
+      //else
+      edgeData += word1+','+word2 +','+med+ "\n";
+
+      if (edgeData.length > 10*1024*1024) {
+         console.log('Appending to: '+edgeFile);
+         fs.appendFileSync(edgeFile, edgeData);
+         edgeData = '';
+      }
     }
   }
   if (i%100==0) console.log(Math.floor((i/keys.length)*1000)/10+'% complete');
