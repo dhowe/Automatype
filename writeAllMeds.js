45982bc5e885483317c24adc8be9a4894dcd5023
dhowe <daniel@rednoise.org>
2018-12-24 12:18:55 +0800

    append-script

--- writeAllMeds.js
+++ writeAllMeds.js
@@ -1,9 +1,7 @@
 let fs = require('fs');
-//let RiTa = require('rita');
-let RiTa = require('./lib/rita-full');
+let RiTa = require('rita');
 let levenshtein = require('fast-levenshtein');
 
-let format = 'cyto';
 let args = process.argv.slice(2);
 let maxAllowedMed = args && Math.min(args[0],3) || 0;
 let keys = new RiTa.RiLexicon().keys;
@@ -16,25 +14,28 @@ if (maxAllowedMed < 1) {
 }
 
 let edgeData = "Source,Target,Med\n";
-FOR: for (var i = 0; i < keys.length; i++) {
+for (var i = 0; i < keys.length; i++) {
   for (var j = 0; j < keys.length; j++) {
+
     if (i == j) continue;
+
     var word1 = keys[i], word2 = keys[j];
+
+    // words 3-7 chars only
+    if (Math.min(word1.length,word2.length) < 3) continue;
+    if (Math.max(word1.length,word2.length) > 7) continue;
+
     var med = levenshtein.get(word1, word2);
     if (med <= maxAllowedMed) {
-      //if (format != 'cyto')
        // edgeData += words[word1]+','+words[word2] + ",undirected\n";
-      //else
       edgeData += word1+','+word2 +','+med+ "\n";
-
-      if (edgeData.length > 10*1024*1024) {
-         console.log('Appending to: '+edgeFile);
-         fs.appendFileSync(edgeFile, edgeData);
-         edgeData = '';
-      }
     }
   }
-  if (i%100==0) console.log(Math.floor((i/keys.length)*1000)/10+'% complete');
+  if (i%100==0) {
+    console.log(Math.floor((i/keys.length)*1000)/10+'% complete');
+    fs.appendFileSync(edgeFile, edgeData);
+    edgeData = '';
+  }
 }
 
 fs.writeFileSync(edgeFile, edgeData);
